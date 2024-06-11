aa1a9ea00d61072fd9fac7b23070ce7759c44dca
Aeoline <raubrey.kh@gmail.com>
2024-06-12 00:31:21 +0700

    tess

--- routes/recipeand.route.js
+++ routes/recipeand.route.js
@@ -7,16 +7,16 @@ const MainController = require("../controllers/MainController");
 const recipeController = new RecipeController;
 const mainController = new MainController;
 
-router.get("/recipes", (req, res) => recipeController.getAllRecipe(req, res));
-router.get("/recipes/:id", (req, res) => recipeController.getRecipeById(req, res));
-router.get("/recipes/:slug", (req, res) => {
+router.get("/recipes_a", (req, res) => recipeController.getAllRecipe(req, res));
+router.get("/recipes_a/:id", (req, res) => recipeController.getRecipeById(req, res));
+router.get("/recipes_a/:slug", (req, res) => {
     recipeController.getRecipeBySlug(req, res);
   }); //blm bisa
-router.get("/recipes/:title", (req, res) => {
+router.get("/recipes_a/:title", (req, res) => {
     recipeController.searchRecipe(req, res);
   });
 
-router.get("/favrecipes/", (req, res) => recipeController.getFavRecipe(req, res));
+router.get("/favrecipes_a/", (req, res) => recipeController.getFavRecipe(req, res));
 
 
 
