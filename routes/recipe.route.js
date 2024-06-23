7ac5e4affedf91963cacba2a68cdd7f430887d84
Aeoline <raubrey.kh@gmail.com>
2024-06-23 21:11:21 +0700

    finish

--- routes/recipe.route.js
+++ routes/recipe.route.js
@@ -1,36 +1,30 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const multer = require("multer");
 const router = express.Router();
 
 const RecipeController = require("../controllers/RecipeController");
 const middleware = require("../middleware/auth.middleware");
 
 const recipeController = new RecipeController();
-const upload = multer({ storage: multer.memoryStorage() });
 
-// Increase payload size limit
-router.use(bodyParser.json({ limit: '50mb' }));
-router.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+router.use((req, res, next) => {
+  console.log("Middleware executed Recipe");
+  next();
+});
 
-router.get("/recipes", middleware.isUserMiddleware, (req, res) => recipeController.getAllRecipe(req, res));
+router.get("/recipes", (req, res) => recipeController.getAllRecipe(req, res));
+
 router.get("/recipes/:id", middleware.isUserMiddleware, (req, res) => recipeController.getRecipeById(req, res));
 router.get("/recipes/:slug", middleware.isUserMiddleware, (req, res) => {
   recipeController.getRecipeBySlug(req, res);
-}); // belum bisa
+}); //belum bisa
 router.get("/recipes/:title", middleware.isUserMiddleware, (req, res) => {
   recipeController.searchRecipe(req, res);
 });
 
-router.use((req, res, next) => {
-  console.log("Middleware executed");
-  next();
-});
-
-router.post("/recipes", [middleware.isUserMiddleware, middleware.isAdminMiddleware, upload.single('photo')], (req, res) => recipeController.addRecipe(req, res));
+router.post("/recipes", [middleware.isUserMiddleware, middleware.isAdminMiddleware], (req, res) => recipeController.addRecipe(req, res));
 router.put("/recipes/:id", [middleware.isUserMiddleware, middleware.isAdminMiddleware], (req, res) => recipeController.updateRecipe(req, res));
-router.delete("/recipes/:id", [middleware.isUserMiddleware, middleware.isAdminMiddleware], (req, res) => recipeController.deleteRecipes(req, res));
+router.delete("/recipes/:id", [middleware.isUserMiddleware, middleware.isAdminMiddleware], (req, res) => recipeController.deleteRecipe(req, res));
 
-router.get("/favrecipes/", middleware.isUserMiddleware, (req, res) => recipeController.getFavRecipe(req, res));
+router.get("/recommended_recipes/", (req, res) => recipeController.getFavRecipe(req, res));
 
 module.exports = router;
