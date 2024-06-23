ee485c5998f847e09d07181f22ff9c79511fcd6e
Ratu Aubrey Khairani <115090223+Aeoline@users.noreply.github.com>
2024-06-23 22:03:28 +0700

    Update recipe.route.js

--- routes/recipe.route.js
+++ routes/recipe.route.js
@@ -1,39 +1,31 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const router = express.Router();
 
-// Set the payload limit to 10MB (adjust as needed)
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
-
-const RecipeController = require("./controllers/RecipeController");
-const middleware = require("./middleware/auth.middleware");
+const RecipeController = require("../controllers/RecipeController");
+const middleware = require("../middleware/auth.middleware");
 
 const recipeController = new RecipeController();
 
-const router = express.Router();
+router.use((req, res, next) => {
+  console.log("Middleware executed Recipe");
+  next();
+});
 
-router.get("/recipes", middleware.isUserMiddleware, (req, res) => recipeController.getAllRecipe(req, res));
+router.get("/recipes", (req, res) => recipeController.getAllRecipe(req, res));
+
 router.get("/recipes/:id", middleware.isUserMiddleware, (req, res) => recipeController.getRecipeById(req, res));
 router.get("/recipes/:slug", middleware.isUserMiddleware, (req, res) => {
   recipeController.getRecipeBySlug(req, res);
-}); // belum bisa
+}); //belum bisa
 router.get("/recipes/:title", middleware.isUserMiddleware, (req, res) => {
   recipeController.searchRecipe(req, res);
-});
-
-router.use((req, res, next) => {
-  console.log("Middleware executed");
-  next();
 });
 
 router.post("/recipes", [middleware.isUserMiddleware, middleware.isAdminMiddleware], (req, res) => recipeController.addRecipe(req, res));
 router.put("/recipes/:id", [middleware.isUserMiddleware, middleware.isAdminMiddleware], (req, res) => recipeController.updateRecipe(req, res));
 router.delete("/recipes/:id", [middleware.isUserMiddleware, middleware.isAdminMiddleware], (req, res) => recipeController.deleteRecipes(req, res));
 
-router.get("/favrecipes/", middleware.isUserMiddleware, (req, res) => recipeController.getFavRecipe(req, res));
 
-app.use('/api', router);
+router.get("/recommended_recipes/", (req, res) => recipeController.getFavRecipe(req, res));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+module.exports = router;
